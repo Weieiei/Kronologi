7a7745e870e30abaea4535ec370d3b19a2c3533b
vartanbeno <vartanbeno@gmail.com>
2018-10-15 00:54:57 -0400

    #4 bind register form data to User object
    also adjusted some styling and prettied up the landing page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  user: User;
+
   constructor() { }
 
   ngOnInit() {
+    this.user = new User();
+  }
+
+  registerUser() {
+    console.log(this.user);
   }
 
 }
