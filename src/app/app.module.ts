c765110109429a3e1627cbad177a16f742797795
vartanbeno <vartanbeno@gmail.com>
2018-10-14 23:08:57 -0400

    #4 create dummy register component
    also added FormsModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppRoutingModule } from './/app-routing.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     MaterialModule
   ],
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,19 +1,24 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatCheckboxModule,
         MatToolbarModule,
-        MatIconModule
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule
     ],
     exports: [
         MatButtonModule,
         MatCheckboxModule,
         MatToolbarModule,
-        MatIconModule
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule
     ]
 })
 export class MaterialModule { }
