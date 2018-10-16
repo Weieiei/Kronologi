1b66f13673b6aa898608e86b0bf100170e23b38d
vartanbeno <vartanbeno@gmail.com>
2018-10-16 13:57:57 -0400

    #4 using bcrypt to hash passwords
    After doing some research, I came to the conclusion that hashing the password within the database is not the most secure thing to do.
    That is because if you do that, you would be sending the password to the database at a certain point in plain text format, which is back because that information will be logged.

--- server/api/routes/authenticate.js
+++ server/api/routes/authenticate.js
@@ -1,32 +1,52 @@
 const express = require('express'),
+    bcrypt = require('bcrypt'),
     db = require('../../db'),
+    saltRounds = 10,
+    passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z]).{6,30}$/,
     authenticate = express.Router();
 
 authenticate.post('/register', (req, res) => {
     const { first_name, last_name, email, username, password } = req.body.user;
 
-    db.query(`
-    INSERT INTO users
-    (first_name, last_name, email, username, password_hash) VALUES
-    ($1, $2, $3, $4, $5)
-    RETURNING user_id;
-    `, [first_name, last_name, email, username, password],
-    (error, result) => {
+    if (password.length < 6 || password.length > 30) {
+        return res.status(400).send({ passwordError: 'Password must be between 6 and 30 characters.' });
+    }
+    else if (!passwordRegex.test(password)) {
+        return res.status(400).send({ passwordError: 'Password must contain at least 1 letter and 1 digit.' });
+    }
 
-        if (error) {
-            switch (error.constraint) {
-                case 'users_email_key':
-                    return res.status(400).send({ emailError: 'An account with this email already exists.' });
-                case 'users_username_key':
-                    return res.status(400).send({ usernameError: 'This username is taken.' });
-            }
-            return res.status(500).send({ error: 'Something went wrong.' });
-        }
-        else {
-            const user_id = result.rows[0].user_id;
-            return res.status(200).send({ user_id });
-        }
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
 
+            db.query(`
+            INSERT INTO users
+            (first_name, last_name, email, username, password) VALUES
+            ($1, $2, $3, $4, $5)
+            RETURNING user_id;
+            `, [first_name, last_name, email, username, hash],
+            (error, result) => {
+
+                if (error) {
+                    switch (error.constraint) {
+                        case 'users_email_key':
+                            return res.status(400).send({ emailError: 'An account with this email already exists.' });
+                        case 'users_username_key':
+                            return res.status(400).send({ usernameError: 'This username is taken.' });
+                    }
+                    return res.status(500).send({ error: 'Something went wrong.' });
+                }
+                else {
+                    const user_id = result.rows[0].user_id;
+                    const message = 'User successfully created.';
+                    return res.status(200).send({
+                        user_id,
+                        message
+                    });
+                }
+
+            })
+
+        })
     })
 })
 
--- server/db/init.sql
+++ server/db/init.sql
@@ -1,6 +1,4 @@
 CREATE EXTENSION IF NOT EXISTS CITEXT;
-CREATE EXTENSION IF NOT EXISTS PGCRYPTO;
-
 
 CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL PRIMARY KEY,
@@ -12,31 +10,6 @@ CREATE TABLE IF NOT EXISTS users(
         CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+[.][A-Za-z]{2,3}$'),
     username CITEXT UNIQUE NOT NULL
         CHECK (char_length(username) >= 4 AND char_length(username) <= 30),
-    password_hash VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     join_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-
-CREATE OR REPLACE FUNCTION hash_password() RETURNS TRIGGER AS
-$$
-BEGIN
-    IF ((SELECT LENGTH(NEW.password_hash) < 6) OR (SELECT LENGTH(NEW.password_hash) > 30))
-    THEN RAISE EXCEPTION 'The password should be 6 to 30 characters long.';
-    END IF;
-    NEW.password_hash := crypt(NEW.password_hash, gen_salt('bf', 8));
-    RETURN NEW;
-END
-$$
-LANGUAGE plpgsql;
-
-
-CREATE TRIGGER hash_password
-BEFORE INSERT ON users
-    FOR EACH ROW EXECUTE PROCEDURE hash_password();
-
-
-CREATE TRIGGER hash_updated_password
-BEFORE UPDATE ON users
-    FOR EACH ROW
-    WHEN (OLD.password_hash IS DISTINCT FROM NEW.password_hash)
-    EXECUTE PROCEDURE hash_password();
