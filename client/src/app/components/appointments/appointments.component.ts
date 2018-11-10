9c85a4de48955d88756adae82a4d9e7ca8f83e95
Unknown <jbk5988@gmail.com>
2018-11-10 18:08:38 -0500

    #56 Added Data Table with Dummy Data for Appointments + Styling Fix
    Added a material design data table for appointments, defined appointment type, added dummy data, modified app component styling to resize based on child y overflow, added Material Table module

--- client/src/app/components/appointments/appointments.component.ts
+++ client/src/app/components/appointments/appointments.component.ts
@@ -2,12 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {HttpClient} from '@angular/common/http';
 
+export interface Appointment {
+  service: string;
+  date: string;
+  time: string;
+  duration: string;
+  user: string;
+}
+
+const APPOINTMENT_DATA: Appointment[] = [
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
+];
+
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.css']
 })
 export class AppointmentsComponent implements OnInit {
+
+  displayedColumns: string[] = ['service', 'date', 'time', 'duration', 'user'];
+  dataSource = APPOINTMENT_DATA;
 
   constructor(private authService: AuthService, private httpService: HttpClient) { }
 
