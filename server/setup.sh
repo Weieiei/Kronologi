80318d4f290460e733403ecf79398d4627857098
vartanbeno <vartanbeno@gmail.com>
2018-11-10 01:42:29 -0500

    Only run knex/node commands if there's a db connection
    Before running the knex/node commands (to migrate the latest db, and run the server), first check if there exists a connection to the db.
    If there is, run the appropriate commands.
    If there isn't, the web-server container will exit.
    
    See #67
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- server/setup.sh
+++ server/setup.sh
@@ -3,12 +3,13 @@
 # Wait for the Postgres db to start
 # If it hasn't started, wait 2 seconds before checking again
 # Do this 10 times max, we don't want the program to hang forever
-limit=10
-counter=0
+
+# If a connection exists, set up tables and start the server
 
 bash -c '
     limit=10
     counter=0
+
     while !</dev/tcp/$DB_HOST/5432
     do
         if [ $counter -eq $limit ]
@@ -20,12 +21,11 @@ bash -c '
         counter=$(($counter+1))
         sleep 2
     done
+
+    if </dev/tcp/$DB_HOST/5432 ;
+    then
+        cd ./src/
+        (cd ./db/ && ../../node_modules/.bin/knex migrate:latest)
+        node app.js
+    fi
 '
-
-cd ./src/
-
-# Set up tables in db
-(cd ./db/ && ../../node_modules/.bin/knex migrate:latest)
-
-# Run the server
-node app.js
