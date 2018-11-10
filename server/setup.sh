454f3f8128e7aa1aaa32561416bb1596583b4d2f
vartanbeno <vartanbeno@gmail.com>
2018-11-10 00:56:39 -0500

    Limit number of times server pings db to 10
    Modified the Bash script a bit to put a max number of attemps that the web server can ping the db for a connection.
    Without this, there's risk of the program hanging forever.
    
    See #67
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- server/setup.sh
+++ server/setup.sh
@@ -2,7 +2,25 @@
 
 # Wait for the Postgres db to start
 # If it hasn't started, wait 2 seconds before checking again
-bash -c 'while !</dev/tcp/$DB_HOST/5432; do sleep 2; done;'
+# Do this 10 times max, we don't want the program to hang forever
+limit=10
+counter=0
+
+bash -c '
+    limit=10
+    counter=0
+    while !</dev/tcp/$DB_HOST/5432
+    do
+        if [ $counter -eq $limit ]
+        then
+            echo "A database connection could not be established."
+            exit 1
+        fi
+
+        counter=$(($counter+1))
+        sleep 2
+    done
+'
 
 cd ./src/
 
