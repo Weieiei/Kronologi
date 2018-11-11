82bcd6c5c01a9cec19a82c2de4af6698af001547
vartanbeno <vartanbeno@gmail.com>
2018-11-10 20:15:29 -0500

    Fix failing test
    Gotta inject HttpClientModule in the test suite for appointment.service, since the actual service uses it.
    Progress for #52 #53
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- client/src/app/services/appointment.service.spec.ts
+++ client/src/app/services/appointment.service.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AppointmentService } from './appointment.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppointmentService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AppointmentService]
+      providers: [AppointmentService],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule
+      ]
     });
   });
 
