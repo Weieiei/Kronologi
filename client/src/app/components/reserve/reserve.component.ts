3a7697c4f5b98c5aa92fb6bc64381c4fcca8b900
Wei He <hewei196126@gmail.com>
2018-11-11 01:51:26 -0500

    #58 trying to add services names from db for the drop-down list

--- client/src/app/components/reserve/reserve.component.ts
+++ client/src/app/components/reserve/reserve.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Service } from 'src/app/models/service/service';
+import { AuthService } from 'src/app/services/auth.service';
 import { Appointment } from 'src/app/models/appointment/appointment';
 import { User } from 'src/app/models/user/user';
 import { Router } from '@angular/router';
@@ -11,11 +12,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./reserve.component.css']
 })
 export class ReserveComponent implements OnInit {
+  appointment: Appointment;
+  service: Service;
+  selectedOption: string;
+  service_options = [
+    { name: 'option1', value: 1 },
+    { name: 'option2', value: 2 } // using getting to get a list of services
+  ];
+  print() {
+    this.service.name = this.selectedOption;
+  }
 
-  constructor() { }
+  constructor(private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
-
+    this.appointment = new Appointment();
+  }
+  reserve_service() {
+    // TODO
   }
 
 }
