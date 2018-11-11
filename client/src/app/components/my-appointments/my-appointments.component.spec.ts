77e1651f1c1f7480084d8604176cd249db51a8c9
vartanbeno <vartanbeno@gmail.com>
2018-11-11 17:57:53 -0500

    Testing that the appointments show up in table
    Feeding the component.appointments array dummy appointments data to test that it shows up.
    For some reason the 'By' class isn't recognizing the <tr> row tags, so I just parse the innerHTML of the whole table, and make sure there are 3 rows that show up (because I put in 3 dummy appointments in the list).
    
    Ref #53
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- client/src/app/components/my-appointments/my-appointments.component.spec.ts
+++ client/src/app/components/my-appointments/my-appointments.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyAppointmentsComponent } from './my-appointments.component';
-import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialModule } from 'src/app/material';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 @Component({ selector: 'app-appointment', template: '' })
 class AppointmentStubComponent { }
@@ -13,6 +14,8 @@ class AppointmentStubComponent { }
 describe('MyAppointmentsComponent', () => {
   let component: MyAppointmentsComponent;
   let fixture: ComponentFixture<MyAppointmentsComponent>;
+
+  let table: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -39,7 +42,24 @@ describe('MyAppointmentsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should not create if user is not logged in', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display n appointments if appointments array has n items', () => {
+
+    component.appointments = [
+      { name: 'service', start_time: '2018-11-14 21:00', duration: 45, notes: 'notes' },
+      { name: 'service', start_time: '2018-11-14 21:00', duration: 45, notes: 'notes' },
+      { name: 'service', start_time: '2018-11-14 21:00', duration: 45, notes: 'notes' }
+    ]
+
+    fixture.detectChanges();
+
+    table = fixture.debugElement.query(By.css('table'));
+    // count the number of tags with attribute class="mat-row"
+    // the header is mat-header-row so it won't show up
+    expect((table.nativeElement.innerHTML.match(/class="mat-row"/g) || []).length).toEqual(component.appointments.length);
+
+  })
 });
