eed4ba51fca49bd86632861bb273b2b2de8c19b6
vartanbeno <vartanbeno@gmail.com>
2018-11-11 18:39:39 -0500

    Only display appointments today or in the future
    Progress for #52 #53
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- server/src/api/routes/appointments/user/user.js
+++ server/src/api/routes/appointments/user/user.js
@@ -7,12 +7,15 @@ const userAppts = express.Router();
 userAppts.get('/', jwtWrapper.verifyToken, (req, res) => {
 
     const userId = req.userId;
+    let today = new Date();
+    today = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`;
 
     knex
         .select('appointments.id', 'service_id', 'services.name', 'start_time', 'end_time', 'duration', 'notes')
         .from('appointments')
         .innerJoin('services', 'appointments.service_id', 'services.id')
         .where('appointments.user_id', userId)
+        .andWhereRaw(`appointments.start_time >= '${today}'::date`)
     .then(appointments => {
         return res.status(200).send({ appointments });
     })
