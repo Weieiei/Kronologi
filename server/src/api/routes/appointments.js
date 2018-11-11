4a0a97271181fc7cb5ddbb284945277a8953055b
vartanbeno <vartanbeno@gmail.com>
2018-11-11 14:08:05 -0500

    GET appointments makes a JOIN clause with services
    When the user sees their appointments, they should get back the service name as well.
    Returning just a service ID is useless to the user and created a bad user experience.
    
    So we make the appointments table JOIN the services table using the related service_id attribute.
    That way, we can easily get the service name for each appointment.
    
    Progress for #52 #53
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- server/src/api/routes/appointments.js
+++ server/src/api/routes/appointments.js
@@ -9,20 +9,11 @@ appointments.get('/user', jwtWrapper.verifyToken, (req, res) => {
 
     const userId = req.userId;
 
-    knex.select().from('appointments').where('user_id', userId)
-    .then(resultSet => {
-
-        const appointments = [];
-
-        resultSet.forEach(row => {
-            const appointment = new Appointment(
-                row.id, row.user_id, row.service_id,row.start_time, row.end_time, row.notes
-            );
-            appointments.push(appointment);
-        });
-
-        return res.status(200).send(appointments);
-
+    knex.select().from('appointments')
+        .innerJoin('services', 'appointments.service_id', 'services.id')
+        .where('appointments.user_id', userId)
+    .then(appointments => {
+        return res.status(200).send({ appointments });
     })
     .catch(error => {
         return res.status(500).send({ error });
