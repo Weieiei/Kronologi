b2c72c39e6ba97e9b7c8a29cf862e5c72a02c93f
Unknown <jbk5988@gmail.com>
2018-11-11 16:05:16 -0500

    #50 Fixed Creation Unit test, fixed typo
    Added missing imports for unit test, fixed typo

--- client/src/app/components/appointments/appointments.component.spec.ts
+++ client/src/app/components/appointments/appointments.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppointmentsComponent } from './appointments.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterTestingModule} from "@angular/router/testing";
+import {MaterialModule} from "../../material";
 
 describe('AppointmentsComponent', () => {
   let component: AppointmentsComponent;
@@ -8,6 +11,11 @@ describe('AppointmentsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        RouterTestingModule,
+        MaterialModule
+      ],
       declarations: [ AppointmentsComponent ]
     })
     .compileComponents();
