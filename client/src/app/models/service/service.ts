d0f4e627f971faedc59a22ba5117903327568283
Wei He <hewei196126@gmail.com>
2018-11-12 15:59:00 -0500

    sloving build failed error

--- client/src/app/models/service/service.ts
+++ client/src/app/models/service/service.ts
@@ -1,40 +1,4 @@
 export class Service {
-
-<<<<<<< HEAD
-  private _id: number;
-  private _name: string;
-  private _duration: number;
-
-  constructor(
-      id: number = null,
-      name: string = '',
-      duration: number = null
-  ) {
-      this._id = id;
-      this._name = name;
-      this._duration = duration;
-  }
-
-  public get id(): number {
-      return this._id;
-  }
-
-  public get name(): string {
-      return this._name;
-  }
-
-  public set name(name: string) {
-      this._name = name;
-  }
-
-  public get duration(): number {
-      return this._duration;
-  }
-
-  public set duration(duration: number) {
-      this._duration = duration;
-  }
-=======
     private _id: number;
     private _name: string;
     private _duration: number;
@@ -44,7 +8,7 @@ export class Service {
     ) {
         this._id = id;
         this._name = name;
-        this._duration = duration
+        this._duration = duration;
     }
 
     public get id(): number {
@@ -66,6 +30,4 @@ export class Service {
     public set duration(duration: number) {
         this._duration = duration;
     }
->>>>>>> a10b033e4c4d6e1dfe25b1180670c0bff0d05031
-
 }
