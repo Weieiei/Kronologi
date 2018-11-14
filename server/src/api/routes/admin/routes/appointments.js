c8ed45454688ed5acb2045cd4ec0d9735eb080bf
vartanbeno <vartanbeno@gmail.com>
2018-11-13 22:01:10 -0500

    Modify DB query to make an inner join
    For the admin to get all appointments, it would be better if they get info like the service name, the user's name, email, etc.
    If not, they're stuck with a plain old service ID, user ID, etc.
    To fix this, we simply join the needed tables on the appropriate related attributes.
    
    See #56
    
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- server/src/api/routes/admin/routes/appointments.js
+++ server/src/api/routes/admin/routes/appointments.js
@@ -4,10 +4,15 @@ const knex = require('../../../../db/knex');
 const appointments = express.Router();
 
 appointments.get('/', (req, res) => {
-    knex.select()
+    knex.select('appointments.id', 'user_id', 'services.name', 'start_time', 'end_time', 'services.duration', 'notes',
+                'users.first_name', 'users.last_name', 'users.email', 'users.username')
         .from('appointments')
+        .innerJoin('users', 'appointments.user_id', 'users.id')
+        .innerJoin('services', 'appointments.service_id', 'services.id')
         .then(appointments => {
-            res.json(appointments);
+
+            res.status(200).send({ appointments });
+            
         });
 });
 
