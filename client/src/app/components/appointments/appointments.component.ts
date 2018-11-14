5acaf74c53aca3f35d35daee7bbf259955f815bc
Unknown <jbk5988@gmail.com>
2018-11-13 22:18:29 -0500

    #50 #56 Updated UI to use Duration, Service and User Name from GET
    Updated UI to use Duration, Service and User Name from updated GET route,
    removed parsing methods that were made unnecessary by the change

--- client/src/app/components/appointments/appointments.component.ts
+++ client/src/app/components/appointments/appointments.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import {HttpClient} from '@angular/common/http';
 import {AppointmentService} from "../../services/appointment/appointment.service";
-import {Service} from "../../interfaces/service";
-import {ServicesService} from "../../services/services/services.service";
 import * as moment from "moment";
 
 export interface Appointment {
@@ -30,50 +28,33 @@ export class AppointmentsComponent implements OnInit {
 
   constructor(private authService: AuthService,
               private httpService: HttpClient,
-              private appointmentService: AppointmentService,
-              private servicesService: ServicesService) { }
+              private appointmentService: AppointmentService) { }
 
   ngOnInit() {
-    this.getServices();
     this.getAllAppointments();
   }
 
   getAllAppointments(): void {
     this.appointmentService.getAllAppointments().subscribe(
-      res => {this.rawData = res;
+      res => {this.rawData = res['appointments'];
         this.populateServiceNames();},
       err => console.log(err)
     );
-  }
-
-  getServices() {
-    this.servicesService.getServices().subscribe(
-      res => this.services = res,
-      err => console.log(err)
-    )
   }
 
   populateServiceNames() {
     const dataSourceParsed: Appointment[] = [];
     this.rawData.forEach(item => {
       const appointment: Appointment = {
-        service: this.findServiceById(item.service_id).name,
+        service: item.name,
         date: this.getDate(item.start_time),
         time: this.getTime(item.start_time),
-        duration: this.getDuration(item.start_time, item.end_time),
-        user: item.user_id.toString(),
+        duration: item.duration + " min.",
+        user: item.first_name + " " + item.last_name,
       };
       dataSourceParsed.push(appointment);
     });
     this.dataSource = dataSourceParsed;
-  }
-
-  getDuration(startTime: Date, endTime: Date) {
-    const start: Date = new Date(startTime);
-    const end: Date = new Date(endTime);
-    // @ts-ignore
-    const duration = Math.abs(end - start) / 36e5 * 60;
-    return duration.toFixed(0) + " min.";
   }
 
   getDate(date: any) {
@@ -84,9 +65,5 @@ export class AppointmentsComponent implements OnInit {
   getTime(date: any) {
     const processedDate = new Date(date);
     return moment(processedDate).format('hh:mm A');
-  }
-
-  private findServiceById(id: number): Service {
-    return this.services.find(service => service.id == id);
   }
 }
