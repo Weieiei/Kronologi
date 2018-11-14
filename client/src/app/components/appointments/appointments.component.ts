68f01080ea8199e3674458223b56fa0c2e3676cc
Unknown <jbk5988@gmail.com>
2018-11-13 20:08:02 -0500

    #50 Added Admin Appointments get, String parsing, Service Name Get
    Added all appointments as admin getting functionality, added string parsing for server response data, added method to get service name, renamed the feature in the navbar

--- client/src/app/components/appointments/appointments.component.ts
+++ client/src/app/components/appointments/appointments.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import {HttpClient} from '@angular/common/http';
 import {AppointmentService} from "../../services/appointment/appointment.service";
+import {Service} from "../../interfaces/service";
+import {ServicesService} from "../../services/services/services.service";
+import * as moment from "moment";
 
 export interface Appointment {
   service: string;
@@ -11,19 +14,6 @@ export interface Appointment {
   user: string;
 }
 
-const APPOINTMENT_DATA: Appointment[] = [
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-  {service: 'A service', date: '2018-11-10', time: '5:00 PM', duration: '1 hour', user: 'A user'},
-];
-
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
@@ -32,28 +22,71 @@ const APPOINTMENT_DATA: Appointment[] = [
 export class AppointmentsComponent implements OnInit {
 
   displayedColumns: string[] = ['service', 'date', 'time', 'duration', 'user'];
-  dataSource = APPOINTMENT_DATA;
+  dataSource: Appointment[] = [];
+  rawData: any[];
+  services: any[];
+
+
 
   constructor(private authService: AuthService,
               private httpService: HttpClient,
-              private appointmentService: AppointmentService) { }
+              private appointmentService: AppointmentService,
+              private servicesService: ServicesService) { }
 
   ngOnInit() {
+    this.getServices();
     this.getAllAppointments();
-  }
-
-  clicked() {
-    console.log('clicked');
-    this.httpService.post('api/appointments', { token: this.authService.getToken()}).subscribe(res => {
-      console.log(res);
-    });
   }
 
   getAllAppointments(): void {
     this.appointmentService.getAllAppointments().subscribe(
-      res => console.log(res)
-        //this.appointments = res['appointments'],
-      //err => console.log(err)
+      res => {this.rawData = res;
+        this.populateServiceNames();},
+      err => console.log(err)
     );
+  }
+
+  getServices() {
+    this.servicesService.getServices().subscribe(
+      res => this.services = res,
+      err => console.log(err)
+    )
+  }
+
+  populateServiceNames() {
+    const dataSourceParsed: Appointment[] = [];
+    this.rawData.forEach(item => {
+      const appointment: Appointment = {
+        service: this.findServiceById(item.service_id).name,
+        date: this.getDate(item.start_time),
+        time: this.getTime(item.start_time),
+        duration: this.getDuration(item.start_time, item.end_time),
+        user: item.user_id.toString(),
+      };
+      dataSourceParsed.push(appointment);
+    });
+    this.dataSource = dataSourceParsed;
+  }
+
+  getDuration(startTime: Date, endTime: Date) {
+    const start: Date = new Date(startTime);
+    const end: Date = new Date(endTime);
+    // @ts-ignore
+    const duration = Math.abs(end - start) / 36e5;
+    return duration.toFixed(2) + " hours";
+  }
+
+  getDate(date: any) {
+    const processedDate = new Date(date);
+    return moment(processedDate).format('YYYY-MM-DD');
+  }
+
+  getTime(date: any) {
+    const processedDate = new Date(date);
+    return moment(processedDate).format('hh:mm A');
+  }
+
+  private findServiceById(id: number): Service {
+    return this.services.find(service => service.id == id);
   }
 }
