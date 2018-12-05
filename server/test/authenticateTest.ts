fd7a7026fc408b16cc8e653039d7fafa87202865
meliduong13 <meliduong13@gmail.com>
2018-12-05 11:42:36 -0500

    #71 test for login and registration best case scenario, still need to delete new user from test db after the test is done

--- server/test/authenticateTest.ts
+++ server/test/authenticateTest.ts
@@ -1,25 +1,25 @@
-//process.env.NODE_ENV = "test";
-var chai = require('chai'), chaiHttp = require('chai-http');
-
-chai.use(chaiHttp);
-
-const expect = chai.expect;
-
-describe('registration best case scenario', () => {
-
-    it('should return status 200 on call', () => {
-        return chai.request('http://localhost:3000/')
-            .post('api/authenticate/login')
-            .type('form')
-            .send({
-                "username" : "test",
-                "password" : "test123"
-            })
-            .then(res => {
-                chai.expect(res.status).to.eql(200);
-            });
-    });
-    //   this.connector = new Connection().knex();
-    // this.connector().table('users').del().where('_username',"melissa");
-
-});
+// //process.env.NODE_ENV = "test";
+// var chai = require('chai'), chaiHttp = require('chai-http');
+//
+// chai.use(chaiHttp);
+//
+// const expect = chai.expect;
+//
+// describe('registration best case scenario', () => {
+//
+//     it('should return status 200 on call', () => {
+//         return chai.request('http://localhost:3000/')
+//             .post('api/authenticate/login')
+//             .type('form')
+//             .send({
+//                 "username" : "test",
+//                 "password" : "test123"
+//             })
+//             .then(res => {
+//                 chai.expect(res.status).to.eql(200);
+//             });
+//     });
+//     //   this.connector = new Connection().knex();
+//     // this.connector().table('users').del().where('_username',"melissa");
+//
+// });
--- server/test/test.ts
+++ server/test/test.ts
@@ -1,13 +1,13 @@
-//process.env.NODE_ENV = "test";
+export {};
+import 'mocha';
 import {Connection} from "../src/db/knex";
-
-var chai = require('chai'), chaiHttp = require('chai-http');
+const chai = require('chai'), chaiHttp = require('chai-http');
+const knex = require('../src/db/knex');
+const express = require('express');
 
 chai.use(chaiHttp);
 
-const expect = chai.expect;
-
-describe('registration with correct credentials', () => {
+describe('login with correct credentials', () => {
 
     it('should return status 200 on call', () => {
         return chai.request('http://localhost:3000/')
@@ -21,7 +21,22 @@ describe('registration with correct credentials', () => {
                 chai.expect(res.status).to.eql(200);
             });
     });
-   this.connector = new Connection().knex();
-this.connector().table('users').del().where('_username',"melissa");
+//    this.connector = new Connection().knex();
+//this.connector().table('users').del().where('_username',"melissa");
 
 });
+
+describe('registration with ', () => {
+
+    it('should return status 200 on call', () => {
+        return chai.request('localhost:3000/')
+            .post('api/authenticate/register')
+            .send({"user":{"_firstName":"lova","_lastName":"duong","_email":"gee@123.com","_username":"gee","_password":"test123"}})
+            .then(res => {
+                chai.expect(res.status).to.eql(200);
+            })
+    })
+    //  this.connector = new Connection().knex();
+    // this.connector().table('users').del().where('_username',"melissa");
+
+})
