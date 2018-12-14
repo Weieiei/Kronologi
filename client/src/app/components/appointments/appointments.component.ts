5313fb8032111defdffe08d902305363d7ab62db
Vartan Benohanian <vartanbeno@gmail.com>
2018-12-14 10:38:27 -0500

    #66: Eager load services when fetching all appointments
    Had to tweak some frontend code to reflect changes
    
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- client/src/app/components/appointments/appointments.component.ts
+++ client/src/app/components/appointments/appointments.component.ts
@@ -1,14 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AppointmentService} from "../../services/appointment/appointment.service";
-import * as moment from "moment";
-
-export interface Appointment {
-  service: string;
-  date: string;
-  time: string;
-  duration: string;
-  user: string;
-}
+import { AppointmentService } from '../../services/appointment/appointment.service';
 
 @Component({
   selector: 'app-appointments',
@@ -18,9 +9,7 @@ export interface Appointment {
 export class AppointmentsComponent implements OnInit {
 
   displayedColumns: string[] = ['service', 'date', 'time', 'duration', 'user'];
-  dataSource: Appointment[] = [];
-  rawData: any[];
-  services: any[];
+  appointments: any[];
 
   constructor(private appointmentService: AppointmentService) { }
 
@@ -30,34 +19,9 @@ export class AppointmentsComponent implements OnInit {
 
   getAllAppointments(): void {
     this.appointmentService.getAllAppointments().subscribe(
-      res => {this.rawData = res['appointments'];
-        this.populateServiceNames();},
+      res => this.appointments = res['appointments'],
       err => console.log(err)
     );
   }
 
-  populateServiceNames() {
-    const dataSourceParsed: Appointment[] = [];
-    this.rawData.forEach(item => {
-      const appointment: Appointment = {
-        service: item.name,
-        date: this.getDate(item.startTime),
-        time: this.getTime(item.startTime),
-        duration: item.duration + " min.",
-        user: item.firstName + " " + item.lastName,
-      };
-      dataSourceParsed.push(appointment);
-    });
-    this.dataSource = dataSourceParsed;
-  }
-
-  getDate(date: any) {
-    const processedDate = new Date(date);
-    return moment(processedDate).format('YYYY-MM-DD');
-  }
-
-  getTime(date: any) {
-    const processedDate = new Date(date);
-    return moment(processedDate).format('hh:mm A');
-  }
 }
--- server/src/api/routes/admin/routes/appointments.ts
+++ server/src/api/routes/admin/routes/appointments.ts
@@ -12,13 +12,7 @@ appointments.get('/', async (req, res) => {
 
     const appointments = await Appointment
         .query()
-        .select(
-            'appointments.*',
-            'users.first_name as first_name', 'users.last_name as last_name', 'users.email as email', 'users.username as username',
-            'services.name as name', 'services.duration as duration'
-        )
-        .join('users', 'appointments.user_id', 'users.id')
-        .join('services', 'appointments.service_id', 'services.id');
+        .eager('[user, service]');
 
     res.status(200).send({ appointments });
 
