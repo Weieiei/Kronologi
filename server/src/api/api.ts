a3d01bafd061b16fd4ec70b869615a8865edd08d
emd21 <32820346+emd21@users.noreply.github.com>
2018-12-17 22:54:06 -0500

    issue #113 task 4: handling appointment conflicts, case 1

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -5,6 +5,7 @@ const services = require('./routes/services');
 
 const user = require('./routes/user/user');
 const admin = require('./routes/admin/admin');
+const appointment = require('./routes/user/routes/appointments');
 
 import { db } from '../db/knex';
 import { Model } from 'objection';
@@ -22,6 +23,7 @@ Model.knex(db);
 
 api.use('/authenticate', authenticate);
 api.use('/services', services);
+api.use('/appointments', appointment);
 
 api.use('/user', userMiddleware, user);
 api.use('/admin', userMiddleware, adminMiddleware, admin);
--- server/src/models/appointment/Appointment.ts
+++ server/src/models/appointment/Appointment.ts
@@ -1,6 +1,6 @@
-import { Model, JsonSchema, RelationMappings, snakeCaseMappers } from 'objection';
+import { Model, JsonSchema, RelationMappings, ValidationError, snakeCaseMappers } from 'objection';
 import { User } from '../user/User';
-import {Service} from "../service/Service";
+import { Service } from '../service/Service';
 
 export class Appointment extends Model {
 
@@ -67,12 +67,33 @@ export class Appointment extends Model {
          * TODO
          * appointment validation
          */
-
+        // appointment validation (making sure there are no conflicts):
+        this.validateNoConflict();
         this.createdAt = new Date();
     }
 
     async $beforeUpdate() {
         this.updatedAt = new Date();
+    }
+
+    async validateNoConflict() {
+
+        const appointment = await Appointment
+            .query().where({startTime: this.startTime}).orWhere({endTime: this.endTime})
+            .first();
+
+        // case 1: there exists an appointment that starts after this appointment and is still ongoing when this appointment ends
+        // (the end time of this appointment is within an already an existing appointment)
+        if (appointment) {
+            if (appointment.startTime >= this.startTime && appointment.startTime <= this.endTime ) {
+                console.log('Conflict: an appointment is starting before your appointment ends.');
+                throw new ValidationError({
+                    message: 'There is an already existing appointment creating a conflict',
+                    type: 'appointment'
+                });
+            }
+
+        }
     }
 
 }
