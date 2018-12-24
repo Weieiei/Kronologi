ab03029da08bc7d3ddcd28e5229a6b5fbf13fdba
emd21 <32820346+emd21@users.noreply.github.com>
2018-12-23 20:17:38 -0500

    fix merge issue

--- server/src/models/appointment/Appointment.ts
+++ server/src/models/appointment/Appointment.ts
@@ -2,7 +2,7 @@ import { Model, JsonSchema, RelationMappings, ValidationError, snakeCaseMappers 
 import { User } from '../user/User';
 import { Service } from '../service/Service';
 import moment from 'moment';
-import {UserType} from "../user/UserType";
+import { UserType } from '../user/UserType';
 
 export class Appointment extends Model {
 
@@ -29,13 +29,13 @@ export class Appointment extends Model {
             type: 'object',
             required: ['userId', 'employeeId', 'serviceId', 'startTime'],
             properties: {
-                id: { type: 'integer' },
-                userId: { type: 'integer' },
-                employeeId: { type: 'integer' },
-                serviceId: { type: 'integer' },
-                startTime: { type: 'string' },
-                endTime: { type: 'string' },
-                notes: { type: 'string' }
+                id: {type: 'integer'},
+                userId: {type: 'integer'},
+                employeeId: {type: 'integer'},
+                serviceId: {type: 'integer'},
+                startTime: {type: 'string'},
+                endTime: {type: 'string'},
+                notes: {type: 'string'}
             }
         };
     }
@@ -66,14 +66,48 @@ export class Appointment extends Model {
     }
 
     async $beforeInsert(queryContext) {
+        /**
+         * Get employee that we chose for the appointment.
+         * If this 'employee' (user) isn't actually an employee, throw an error.
+         * Get the services that this employee offers.
+         * From those services, find the one with the serviceId assigned to the appointment model.
+         * If we get nothing, it means either the employee doesn't offer it, or it just doesn't exist; throw an error.
+         */
+        const employee = await User
+            .query()
+            .where({id: this.employeeId})
+            .first()
+            .eager('[services]');
+
+        if (employee.userType !== UserType.employee) {
+            throw new ValidationError({
+                message: 'You must choose a user of type employee.',
+                type: 'InvalidUser'
+            });
+        }
+
+        const services = employee['services'];
+        const service = services.find(service => service.id === this.serviceId);
+
+        if (!service) {
+            throw new ValidationError({
+                message: 'You must provide a valid service.',
+                type: 'InvalidService'
+            });
+        }
+
+        this.endTime = moment(this.startTime).add(service.duration, 'm').format('YYYY-MM-DD HH:mm:ss');
+        this.startTime += '-05';
+        this.endTime += '-05';
+
         const appointments = await Appointment
-                .query().select();
+            .query().select();
         if (appointments.length > 0) {
             const startTime = new Date(this.startTime);
             const endTime = new Date(this.endTime);
             for (let i = 0; i < appointments.length; i++) {
-                const appointment_start_time =  new Date(appointments[i].startTime);
-                const appointment_end_time =  new Date(appointments[i].endTime);
+                const appointment_start_time = new Date(appointments[i].startTime);
+                const appointment_end_time = new Date(appointments[i].endTime);
                 // case 1: there exists an appointment that starts after this appointment and is still ongoing when this appointment ends
                 // (the end time of this appointment is within an already an existing appointment)
                 if (appointment_start_time <= endTime && appointment_end_time >= endTime) {
@@ -99,50 +133,13 @@ export class Appointment extends Model {
                     });
                 }
             }
-
-        /**
-         * Get employee that we chose for the appointment.
-         * If this 'employee' (user) isn't actually an employee, throw an error.
-         * Get the services that this employee offers.
-         * From those services, find the one with the serviceId assigned to the appointment model.
-         * If we get nothing, it means either the employee doesn't offer it, or it just doesn't exist; throw an error.
-         */
-        const employee = await User
-            .query()
-            .where({ id: this.employeeId })
-            .first()
-            .eager('[services]');
-
-        if (employee.userType !== UserType.employee) {
-            throw new ValidationError({
-                message: 'You must choose a user of type employee.',
-                type: 'InvalidUser'
-            });
+            this.createdAt = new Date();
         }
-
-        const services = employee['services'];
-        const service = services.find(service => service.id === this.serviceId);
-
-        if (!service) {
-            throw new ValidationError({
-                message: 'You must provide a valid service.',
-                type: 'InvalidService'
-            });
-        }
-
-        this.endTime = moment(this.startTime).add(service.duration, 'm').format('YYYY-MM-DD HH:mm:ss');
-        this.startTime += '-05';
-        this.endTime += '-05';
-
-        /**
-         * TODO
-         * appointment validation
-         */
-
-        this.createdAt = new Date();
     }
-
     async $beforeUpdate() {
         this.updatedAt = new Date();
     }
 }
+
+
+
