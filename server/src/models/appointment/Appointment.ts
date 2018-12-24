07c9ec59c095a132e1841e14403c1b9940f53ee6
emd21 <32820346+emd21@users.noreply.github.com>
2018-12-23 20:04:11 -0500

    issue #113 task 4: removed some additional console logs used for testing and also removed some cluttering comments

--- server/src/models/appointment/Appointment.ts
+++ server/src/models/appointment/Appointment.ts
@@ -62,17 +62,8 @@ export class Appointment extends Model {
     }
 
     async $beforeInsert(queryContext) {
-        /**
-         * TODO
-         * appointment validation
-         */
-        // appointment validation (making sure there are no conflicts):
-        // await super.$beforeInsert(queryContext);
-        // const promise = this.validateNoConflict();
         const appointments = await Appointment
                 .query().select();
-        // .query().where({startTime: this.startTime}).orWhere({endTime: this.endTime})
-        // .first();
         if (appointments.length > 0) {
             const startTime = new Date(this.startTime);
             const endTime = new Date(this.endTime);
@@ -82,9 +73,6 @@ export class Appointment extends Model {
                 // case 1: there exists an appointment that starts after this appointment and is still ongoing when this appointment ends
                 // (the end time of this appointment is within an already an existing appointment)
                 if (appointment_start_time <= endTime && appointment_end_time >= endTime) {
-                    console.log('Conflict: (case 1) an appointment is starting before your appointment ends.');
-                    console.log ('Old appointment starts at: ' + appointment_start_time + ' and ends at: ' + appointment_end_time);
-                    console.log ('New appointment starts at: ' + startTime + ' and ends at: ' + endTime);
                     throw new ValidationError({
                         message: 'There is an already existing appointment creating a conflict',
                         type: 'appointment'
@@ -93,9 +81,6 @@ export class Appointment extends Model {
                 // case 2: there exists an appointment that starts before this appointment and ends after the start of this appointment
                 // (the start time of this appointment is within an existing appointment)
                 else if (appointment_start_time <= startTime && appointment_end_time >= startTime) {
-                    console.log('Conflict: (case 2) an appointment is ongoing at the start of your appointment');
-                    console.log ('Old appointment starts at: ' + appointment_start_time + ' and ends at: ' + appointment_end_time);
-                    console.log ('New appointment starts at: ' + startTime + ' and ends at: ' + endTime);
                     throw new ValidationError({
                         message: 'There is an already existing appointment creating a conflict',
                         type: 'appointment'
@@ -104,9 +89,6 @@ export class Appointment extends Model {
                 // case 3: there exists an appointment that starts after this appointment and ends before the end of this appointment
                 // (this appointment "surrounds" an existing appointment)
                 else if (appointment_start_time >= startTime && appointment_end_time <= endTime) {
-                    console.log('Conflict: (case 3) an appointment is ongoing during your appointment');
-                    console.log ('Old appointment starts at: ' + appointment_start_time + ' and ends at: ' + appointment_end_time);
-                    console.log ('New appointment starts at: ' + startTime + ' and ends at: ' + endTime);
                     throw new ValidationError({
                         message: 'There is an already existing appointment creating a conflict',
                         type: 'appointment'
@@ -121,7 +103,4 @@ export class Appointment extends Model {
     async $beforeUpdate() {
         this.updatedAt = new Date();
     }
-
-
-
 }
