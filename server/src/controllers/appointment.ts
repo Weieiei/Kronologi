846e45d68c285304a16d77035a0f374a53154376
Vartan Benohanian <vartanbeno@gmail.com>
2018-12-27 19:56:15 -0500

    #88: Create API call to cancel appointment
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- server/src/controllers/appointment.ts
+++ server/src/controllers/appointment.ts
@@ -1,6 +1,7 @@
 import { Appointment } from '../models/appointment/Appointment';
 import { RequestWrapper } from '../wrappers/RequestWrapper';
 import { ValidationError } from 'objection';
+import { AppointmentStatus } from '../models/appointment/AppointmentStatus';
 
 export const getMyAppointments = async (req: RequestWrapper, res) => {
 
@@ -18,8 +19,7 @@ export const getMyAppointments = async (req: RequestWrapper, res) => {
 
         return res.status(200).send(appointments);
 
-    }
-    catch (error) {
+    } catch (error) {
         return res.status(500).send({ error });
     }
 
@@ -41,8 +41,7 @@ export const bookAppointment = async (req: RequestWrapper, res) => {
 
         res.status(200).send({ message: 'Successfully booked.' });
 
-    }
-    catch (error) {
+    } catch (error) {
 
         if (error instanceof ValidationError) {
             return res.status(400).send({ error: error.message });
@@ -51,6 +50,30 @@ export const bookAppointment = async (req: RequestWrapper, res) => {
             res.status(500).send({ error });
         }
 
+    }
+
+};
+
+export const cancelAppointment = async (req: RequestWrapper, res) => {
+
+    const userId: number = req.userId;
+    const { id } = req.params;
+
+    try {
+
+        const appointment = await Appointment.query()
+            .patch({ status: AppointmentStatus.cancelled })
+            .where({ id, clientId: userId, status: AppointmentStatus.confirmed })
+            .first();
+
+        if (appointment) {
+            res.status(200).send({ message: 'Successfully cancelled appointment.' });
+        } else {
+            res.status(400).send({ error: 'Appointment not found.' });
+        }
+
+    } catch (error) {
+        res.status(500).send({ error });
     }
 
 };
