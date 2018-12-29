2de723042c4b1bfb8d2f70015fd21d397dd5fe42
meliduong13 <meliduong13@gmail.com>
2018-12-29 16:58:17 -0500

    #71 completed test for get api/services
     - this test compares the data retrieved from the database and ensures
     it matches the data returned from the get request
     - makes sure return status is 200

--- server/test/servicesTest.ts
+++ server/test/servicesTest.ts
@@ -1,28 +1,27 @@
-// process.env.NODE_ENV = "test";
-// import 'mocha';
-// import {db} from "../src/db/knex";
-//
-// const chai = require('chai'), chaiHttp = require('chai-http');
-// const express = require('express');
-// chai.use(chaiHttp);
-//
-// describe('this set of tests will verify the functionality of GET api/services', () => {
-//     let connector = db;
-//     it('login should return status 401 if password for existing username is wrong', () => {
-//         return chai.request('http://localhost:3000/')
-//             .get('api/services')
-//             .then(res => {
-//                 let resArray=Array.of(res.body)[0];
-//                 console.log("****");
-//                 console.log(resArray[0]["id"]);
-//                 (connector.select().from('services').then((data) => {
-//                 let dbArray=Array.of(data)[0];
-//                // console.log(Array.of(data));
-//             //    console.log(dbArray[0]);
-//                 }));
-//                // chai.expect(res.status).to.eql(401);
-//               //  chai.expect(JSON.parse(res.text).invalidCredentials == ("Incorrect username and/or password."));
-//             });
-//     });
-//
-// });
+process.env.NODE_ENV = "test";
+import 'mocha';
+import {db} from "../src/db/knex";
+
+const chai = require('chai'), chaiHttp = require('chai-http');
+const express = require('express');
+chai.use(chaiHttp);
+
+describe('this set of tests will verify the functionality of GET api/services', () => {
+    it('should return status 200 on call and match the data retrieved directly from the test database', () => {
+        return chai.request('http://localhost:3000/')
+            .get('api/services')
+            .then(res => {
+                let index = 0;
+                (db.select().from('services').then((data) => {
+                    for (let each of res.body) {
+                        chai.expect(each.id).to.equal(data[index].id);
+                        chai.expect(each.name).to.equal(data[index].name);
+                        chai.expect(each.duration).to.equal(data[index].duration);
+                        index++;
+                    }
+                }));
+                chai.expect(res.status).to.eql(200);
+            });
+    });
+
+});
