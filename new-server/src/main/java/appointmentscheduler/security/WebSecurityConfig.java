18251e02c3c6a16724cc83a7114a45623ecae3b2
Matthew Teolis <matthewteolis@gmail.com>
2019-01-07 21:52:34 -0500

    #123 Activated CORS
    It wasn't actually being used before.
    Before this, the backend wasn't reading the Authorization token that was trying to be sent from the frontend.
    
    Signed-off-by: Matthew Teolis <matthewteolis@gmail.com>

--- new-server/src/main/java/appointmentscheduler/security/WebSecurityConfig.java
+++ new-server/src/main/java/appointmentscheduler/security/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
+                .cors().and()
                 .authorizeRequests()
                 .antMatchers("/api/user/login", "/api/user/register").permitAll()
                 .anyRequest().authenticated()
