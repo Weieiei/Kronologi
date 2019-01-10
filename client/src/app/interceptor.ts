536a25bddccfb0e61ccbd918eb87a0f1f49b4cee
Matthew Teolis <matthewteolis@gmail.com>
2019-01-09 23:23:25 -0500

    #99 Interceptor inject UserService via constructor
    Signed-off-by: Matthew Teolis <matthewteolis@gmail.com>

--- client/src/app/interceptor.ts
+++ client/src/app/interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -7,7 +7,7 @@ import { UserService } from './services/user/user.service';
 @Injectable()
 export class UrlInterceptor implements HttpInterceptor {
 
-    constructor(private injector: Injector) { }
+    constructor(private userService: UserService) { }
 
     /**
      * This function will be called for all HTTP calls.
@@ -17,15 +17,13 @@ export class UrlInterceptor implements HttpInterceptor {
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
 
-        const userService = this.injector.get(UserService);
-
         // Forward all api/ paths to server path.
         if (request.url.startsWith('api/')) {
             const url = environment.baseUrl;
             request = request.clone({
                 url: url + request.url,
                 setHeaders: {
-                    Authorization: `Bearer ${userService.getToken()}`
+                    Authorization: `Bearer ${this.userService.getToken()}`
                 }
             });
         }
