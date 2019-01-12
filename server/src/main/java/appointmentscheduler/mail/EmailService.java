2a4e58a3de272a211069e17e74f95f0beb0bda3a
dylanfernandes <dylangfernandes@gmail.com>
2019-01-12 00:20:03 -0500

    #128 setup app logo to be embedded in email sent

--- server/src/main/java/appointmentscheduler/mail/EmailService.java
+++ server/src/main/java/appointmentscheduler/mail/EmailService.java
@@ -5,12 +5,17 @@ package appointmentscheduler.mail;
 
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class EmailService{
     @Autowired
@@ -26,13 +31,14 @@ public class EmailService{
         props.put("mail.smtp.port", "587");
     }
     public void sendmail() throws AddressException, MessagingException, IOException {
-
+        Resource resource = new ClassPathResource("");
+        System.out.println(resource.getFile().getAbsolutePath());
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication("schedulerTester123@outlook.com", "testing123");
             }
         });
-        Message msg = new MimeMessage(session);
+        MimeMessage msg = new MimeMessage(session);
         msg.setFrom(new InternetAddress("schedulerTester123@outlook.com", false));
 
         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("schedulerTester123@outlook.com"));
@@ -45,11 +51,18 @@ public class EmailService{
 
         Multipart multipart = new MimeMultipart();
         multipart.addBodyPart(messageBodyPart);
-        MimeBodyPart attachPart = new MimeBodyPart();
+       // MimeBodyPart attachPart = new MimeBodyPart();
 
-        attachPart.attachFile("src/assets/images/asapp_logo.png");
-        multipart.addBodyPart(attachPart);
-        msg.setContent(multipart);
+        //attachPart.attachFile("src/assets/images/asapp_logo.png");
+        //multipart.addBodyPart(attachPart);
+        MimeMessageHelper helper = new MimeMessageHelper( msg,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        helper.addAttachment("asapp_logo.png", new File("src/assets/images/asapp_logo.png"));
+        String inlineImage = "<img src=\"cid:asapp_logo.png\"></img><br/>";
+        helper.setText(messageBodyPart.getContent()+ inlineImage, true);
+        //msg.setContent(multipart);
         Transport.send(msg);
     }
 }
