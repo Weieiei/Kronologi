beb06814fe3b7b182fd39dd1ed7e75c7d8cc7c71
Vartan Benohanian <vartanbeno@gmail.com>
2019-01-18 00:55:46 -0500

    Delete unneeded line
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- server/src/main/java/appointmentscheduler/AppointmentScheduler.java
+++ server/src/main/java/appointmentscheduler/AppointmentScheduler.java
@@ -1,16 +1,13 @@
 package appointmentscheduler;
 
-import appointmentscheduler.property.TwilioProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class AppointmentScheduler {
     public static void main(String[] args) {
-        new AnnotationConfigApplicationContext(TwilioProperties.class);
         SpringApplication.run(AppointmentScheduler.class, args);
     }
 }
