ac70acdfa1feda380b77e4a3efdfedfceeb0a8dc
meliduong13 <meliduong13@gmail.com>
2019-01-20 12:38:21 -0500

    Cleanup imports and code formatting

--- server/src/test/java/appointmentscheduler/controller/rest/UserControllerTest.java
+++ server/src/test/java/appointmentscheduler/controller/rest/UserControllerTest.java
@@ -5,35 +5,23 @@ import appointmentscheduler.dto.user.UserLoginDTO;
 import appointmentscheduler.entity.appointment.Appointment;
 import appointmentscheduler.entity.service.Service;
 import appointmentscheduler.entity.user.User;
-import appointmentscheduler.repository.UserRepository;
 import appointmentscheduler.service.AuthenticationService;
 import appointmentscheduler.service.appointment.AppointmentService;
 import appointmentscheduler.service.user.UserService;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-import org.hibernate.query.criteria.internal.expression.function.CurrentDateFunction;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -49,13 +37,9 @@ import java.util.List;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 //@WebMvcTest(UserController.class)
@@ -77,7 +61,6 @@ public class UserControllerTest {
 
     @MockBean
     private AuthenticationService authenticationService;
-
 
 
     private MockMvc mockMvc;
@@ -133,7 +116,7 @@ public class UserControllerTest {
         Jackson2ObjectMapperBuilder.json()
                 .featuresToDisable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
         User mockUser = new User("firstName", "lastName", "email@gmail.com", "password");
-        User mockEmployee = new User ("employee", "employeeLastName", "employee@gmail.com", "password2");
+        User mockEmployee = new User("employee", "employeeLastName", "employee@gmail.com", "password2");
         Service service = new Service("serviceName", 20);
         LocalDate date = LocalDate.now();
         LocalTime startTime = LocalTime.now();
@@ -153,22 +136,9 @@ public class UserControllerTest {
 
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
-//        ObjectMapper mapper = new ObjectMapper()
-//                .registerModule(new ParameterNamesModule())
-//                .registerModule(new Jdk8Module())
-//                .registerModule(new JavaTimeModule());
-//        mapper.findAndRegisterModules();
-
         System.out.println((result.getResponse().getContentAsString()));
 
-
-
-
-
-//        JSONAssert.assertEquals(expected, result.getResponse()
-//                .getContentAsString(), false);
     }
-
 
 
 }
