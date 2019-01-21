99545a28400de0bcb088fbced2367650b84294ae
Vartan Benohanian <vartanbeno@gmail.com>
2019-01-21 17:23:17 -0500

    Revert "Show all appointments for client"

--- server/src/main/java/appointmentscheduler/controller/rest/UserController.java
+++ server/src/main/java/appointmentscheduler/controller/rest/UserController.java
@@ -2,19 +2,17 @@ package appointmentscheduler.controller.rest;
 
 import appointmentscheduler.dto.user.UserLoginDTO;
 import appointmentscheduler.dto.user.UserRegisterDTO;
-import appointmentscheduler.entity.appointment.Appointment;
-import appointmentscheduler.service.AuthenticationService;
-import appointmentscheduler.service.appointment.AppointmentService;
 import appointmentscheduler.service.email.EmailService;
 import appointmentscheduler.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-import java.util.List;
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.Map;
@@ -24,17 +22,12 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
-
-    private final AppointmentService appointmentService;
-    private final AuthenticationService authenticationService;
     private final EmailService emailService;
+
     @Autowired
-    public UserController(UserService userService, AppointmentService appointmentService, AuthenticationService authenticationService, EmailService emailService) {
+    public UserController(UserService userService, EmailService emailService) {
         this.userService = userService;
         this.emailService = emailService;
-        this.appointmentService = appointmentService;
-        this.authenticationService = authenticationService;
-
     }
 
     @PostMapping("/register")
@@ -58,10 +51,4 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
     }
-
-    @GetMapping("/current/appointments")
-    public List<Appointment> findByCurrentUser() {
-        return appointmentService.findByClientId(authenticationService.getCurrentUserId());
-    }
-
 }
