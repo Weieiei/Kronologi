e3482261d0bef52c37591f4e715052346ac8cd31
Matthew Teolis <matthewteolis@gmail.com>
2019-01-22 23:31:26 -0500

    #51 Backend for adding appointments is working
    Signed-off-by: Matthew Teolis <matthewteolis@gmail.com>

--- server/src/main/java/appointmentscheduler/controller/rest/AppointmentController.java
+++ server/src/main/java/appointmentscheduler/controller/rest/AppointmentController.java
@@ -1,29 +1,55 @@
 package appointmentscheduler.controller.rest;
 
-import appointmentscheduler.converters.appointment.AppointmentDTOToAppointment;
 import appointmentscheduler.dto.appointment.AppointmentDTO;
 import appointmentscheduler.entity.appointment.Appointment;
+import appointmentscheduler.entity.service.Service;
+import appointmentscheduler.entity.user.Employee;
+import appointmentscheduler.entity.user.User;
+import appointmentscheduler.exception.ResourceNotFoundException;
+import appointmentscheduler.repository.EmployeeRepository;
+import appointmentscheduler.repository.ServiceRepository;
+import appointmentscheduler.repository.UserRepository;
 import appointmentscheduler.service.AuthenticationService;
 import appointmentscheduler.service.appointment.AppointmentService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/${rest.api.path}/appointments")
 public class AppointmentController extends IRestController<Appointment, AppointmentDTO> {
 
-    @Autowired
-    private AppointmentService appointmentService;
+    private final AppointmentService appointmentService;
+
+    private final AuthenticationService authenticationService;
+
+    private final UserRepository userRepository;
+
+    private final EmployeeRepository employeeRepository;
+
+    private final ServiceRepository serviceRepository;
+
+    private ModelMapper modelMapper;
 
     @Autowired
-    private AppointmentDTOToAppointment appointmentConverter;
+    public AppointmentController(AppointmentService appointmentService, AuthenticationService authenticationService, UserRepository userRepository, ServiceRepository serviceRepository, ModelMapper modelMapper, EmployeeRepository employeeRepository) {
+        this.appointmentService = appointmentService;
+        this.authenticationService = authenticationService;
+        this.userRepository = userRepository;
+        this.serviceRepository = serviceRepository;
+        this.modelMapper = modelMapper;
+        this.employeeRepository = employeeRepository;
 
-    @Autowired
-    private AuthenticationService authenticationService;
+        this.modelMapper.addMappings(new PropertyMap<AppointmentDTO, Appointment>() {
+            protected void configure() {
+                skip().setId(0);
+            }
+        });
+    }
 
     @GetMapping
     @Override
@@ -40,17 +66,27 @@ public class AppointmentController extends IRestController<Appointment, Appointm
     @PostMapping
     @Override
     public Appointment add(@RequestBody AppointmentDTO appointmentDTO) {
-        long userId = authenticationService.getCurrentUserId();
+        long clientId = authenticationService.getCurrentUserId();
 
-        Appointment appointment = appointmentConverter.convert(appointmentDTO);
+        Appointment appointment = modelMapper.map(appointmentDTO, Appointment.class);
+
+        User client = userRepository.findById(clientId).orElseThrow(ResourceNotFoundException::new);
+        appointment.setClient(client);
+
+        Employee employee = employeeRepository.findById(appointmentDTO.getEmployeeId()).orElseThrow(ResourceNotFoundException::new);
+        appointment.setEmployee(employee);
+
+        Service service = serviceRepository.findById(appointmentDTO.getServiceId()).orElseThrow(ResourceNotFoundException::new);
+        appointment.setService(service);
+
         return appointmentService.add(appointment);
     }
 
     @PutMapping("/{id}")
     @Override
     public Appointment update(@PathVariable long id, @RequestBody AppointmentDTO appointmentDTO) {
-        Appointment appointment = appointmentConverter.convert(appointmentDTO);
-        return appointmentService.update(id, appointment);
+        // todo not implemented
+        return null;
     }
 
     @DeleteMapping("/{id}")
