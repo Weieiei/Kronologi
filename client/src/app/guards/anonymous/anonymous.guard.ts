0261dce757c61de46551a321d2cb775f4d9f64a7
Vartan Benohanian <vartanbeno@gmail.com>
2019-02-01 13:06:33 -0500

    Move each guard into its specific folder
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- client/src/app/guards/anonymous/anonymous.guard.ts
+++ client/src/app/guards/anonymous/anonymous.guard.ts
@@ -1,27 +1,22 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { UserService } from '../services/user/user.service';
+import { UserService } from '../../services/user/user.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class EmployeeGuard implements CanActivate {
+export class AnonymousGuard implements CanActivate {
 
-    constructor(
-        private userService: UserService,
-        private router: Router
-    ) {
+    constructor(private userService: UserService,
+                private router: Router) {
     }
 
     canActivate(): boolean {
-
-        if (this.userService.isEmployee()) {
+        if (!this.userService.isLoggedIn()) {
             return true;
         }
 
         this.router.navigate(['']);
         return false;
-
     }
-
 }
