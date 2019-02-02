93d539e7a57f6b497a4d253a3ba538b3700be8fa
dylanfernandes <dylangfernandes@gmail.com>
2019-02-02 17:13:06 -0500

    #152 Created DTO for employee shifts
    Signed-off-by: dylanfernandes <dylangfernandes@gmail.com>

---
+++ server/src/main/java/appointmentscheduler/dto/employee/EmployeeShiftDTO.java
@@ -0,0 +1,77 @@
+package appointmentscheduler.dto.employee;
+
+public class EmployeeShiftDTO {
+
+    private long employeeId;
+    private int year;
+    private int month;
+    private int day;
+    private int startHour;
+    private int startMinute;
+    private int endHour;
+    private int endMinute;
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public int getStartMinute() {
+        return startMinute;
+    }
+
+    public int getEndHour() {
+        return endHour;
+    }
+
+    public int getEndMinute() {
+        return endMinute;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setStartHour(int startHour) {
+        this.startHour = startHour;
+    }
+
+    public void setStartMinute(int startMinute) {
+        this.startMinute = startMinute;
+    }
+
+    public void setEndHour(int endHour) {
+        this.endHour = endHour;
+    }
+
+    public void setEndMinute(int endMinute) {
+        this.endMinute = endMinute;
+    }
+}
