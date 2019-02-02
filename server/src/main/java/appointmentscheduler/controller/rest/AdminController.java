a0890defad1c410c7ddec012053d55078b090a4f
dylanfernandes <dylangfernandes@gmail.com>
2019-02-02 18:16:28 -0500

    #152 set up admin route for creating employee shifts
    Signed-off-by: dylanfernandes <dylangfernandes@gmail.com>

--- server/src/main/java/appointmentscheduler/controller/rest/AdminController.java
+++ server/src/main/java/appointmentscheduler/controller/rest/AdminController.java
@@ -1,20 +1,44 @@
 package appointmentscheduler.controller.rest;
 
+import appointmentscheduler.dto.employee.EmployeeShiftDTO;
+import appointmentscheduler.service.employee.EmployeeShiftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("${rest.api.path}/admin")
 @PreAuthorize("hasAuthority('ADMIN')")
 public class AdminController {
 
+    private final EmployeeShiftService employeeShiftService;
+
+    @Autowired
+    AdminController (EmployeeShiftService employeeShiftService) {
+        this.employeeShiftService = employeeShiftService;
+    }
+
     // TODO remove this
     @GetMapping
     public String areYouAnAdmin(@RequestAttribute long userId) {
         return String.format("You are an admin, your id is %d.", userId);
+    }
+
+    @PostMapping("/createShift")
+    public String register(@RequestBody EmployeeShiftDTO employeeShiftDTO) throws IOException, MessagingException {
+        try {
+            return employeeShiftService.createShfit(employeeShiftDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.CONFLICT).build().toString();
+        }
     }
 
 }
