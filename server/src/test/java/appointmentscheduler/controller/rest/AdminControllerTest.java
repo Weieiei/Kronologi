52a005c59f459e65e67c472c5a6881bb18fc44ec
Vartan Benohanian <vartanbeno@gmail.com>
2019-02-02 19:31:06 -0500

    Add `@WithMockUser` to bypass `@PreAuthorize` in tests
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- server/src/test/java/appointmentscheduler/controller/rest/AdminControllerTest.java
+++ server/src/test/java/appointmentscheduler/controller/rest/AdminControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -55,6 +56,7 @@ public class AdminControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
     public void changeRoleToEmployee() throws Exception {
         User mockUser = mock(User.class);
         Set<Role> roles = new HashSet<>();
@@ -69,6 +71,7 @@ public class AdminControllerTest {
         MvcResult result = mockMvc.perform(
                 post("/api/admin/change_to_employee/")
                         .with(request -> {
+                            request.setAttribute("userId", 2);
                             request.setAttribute("id", 1);
                             request.setAttribute("name", "toto");
                             request.setAttribute("duration", 99999);
