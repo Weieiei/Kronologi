2611afbeed3844399453d883ebddd0cb4535c633
MarcLeclair <marc.leclair0113@gmail.com>
2019-03-01 19:15:31 -0500

    #7 created srevices page

--- client/src/app/pages/business-view/business-view.component.ts
+++ client/src/app/pages/business-view/business-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {MatDialog, MatDialogConfig} from "@angular/material";
 @Component({
   selector: 'app-business-view',
   templateUrl: './business-view.component.html',
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class BusinessViewComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private dialog: MatDialog) { }
 
   ngOnInit() {
   }
@@ -16,12 +16,13 @@ export class BusinessViewComponent implements OnInit {
   getAllAppointmentsForUserForBusiness(businessId: number){
     //TODO CALL GET ALL APPOINTMENTS BY BUSINESS ID GOTTEN FROM CARD -- HAVE TO WAIT FOR GET ALL BUSINESS ROUTE FROM BACKEND
     //TEMP FIX TO FIX LAYOUT
-    console.log("bye")
     this.router.navigate(['home']);
   }
 
-  bookAppointmentForBusiness(businessId:number){
-    //TODO GET BUSINESS APPOINTMENT FROM CARD AND CALL THIS FUNCTION -- HAVE TO WAIT FOR GET ALL BUSINESS ROUTE FROM BACKEND
+  openModalForService(businessId:number){
+    //TODO GET BUSINESS SERVICES FROM CARD AND CALL THIS FUNCTION -- HAVE TO WAIT FOR GET ALL BUSINESS ROUTE FROM BACKEND
+
+    }
   }
 
-}
+
