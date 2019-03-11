c4624ca842e9e60a1f9b717150fff3c3fa11d0f6
emd21 <32820346+emd21@users.noreply.github.com>
2019-03-10 22:58:41 -0400

    #26 Adding the resizeImage method

--- client/src/app/services/image-resize/image.resize.service.ts
+++ client/src/app/services/image-resize/image.resize.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {ElementRef, Injectable, ViewChild} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageResizeService {
 
+    @ViewChild('myCanvas') myCanvas: ElementRef;
+    public context: CanvasRenderingContext2D;
   constructor() { }
+
+  resizeImage(img, width, height) {
+      this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
+      this.context.drawImage(img, 0, 0, width, height);
+      return this.context.canvas.toDataURL();
+  }
 }
+
+
