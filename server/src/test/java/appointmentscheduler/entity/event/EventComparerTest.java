14225538b1eb57c56053c7a4299fa4d1a2a97594
dylanfernandes <dylangfernandes@gmail.com>
2019-03-15 15:51:24 -0400

    #33 added hour and minute tests
    Signed-off-by: dylanfernandes <dylangfernandes@gmail.com>

--- server/src/test/java/appointmentscheduler/entity/event/EventComparerTest.java
+++ server/src/test/java/appointmentscheduler/entity/event/EventComparerTest.java
@@ -40,4 +40,48 @@ public class EventComparerTest {
         //event 2 less than event 1
         assertEquals(1, result);
     }
+
+    @Test
+    public void compareLessThanHour() {
+        event1 = new EventTest(LocalTime.of(1,0), LocalTime.of(2,0), LocalDate.of(2018,1, 2));
+        event2 = new EventTest(LocalTime.of(2,0), LocalTime.of(3,0), LocalDate.of(2018,1, 2));
+        int result = eventComparer.compare(event1,event2);
+        //events not conflicting
+        assertFalse(0 == result);
+        //event 1 less than event 2
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void compareGreaterThanHour() {
+        event1 = new EventTest(LocalTime.of(2,0), LocalTime.of(3,0), LocalDate.of(2018,1, 2));
+        event2 = new EventTest(LocalTime.of(1,0), LocalTime.of(2,0), LocalDate.of(2018,1, 2));
+        int result = eventComparer.compare(event1,event2);
+        //events not conflicting
+        assertFalse(0 == result);
+        //event 2 less than event 1
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void compareLessThanMinute() {
+        event1 = new EventTest(LocalTime.of(1,0), LocalTime.of(1,58), LocalDate.of(2018,1, 2));
+        event2 = new EventTest(LocalTime.of(1,59), LocalTime.of(2,0), LocalDate.of(2018,1, 2));
+        int result = eventComparer.compare(event1,event2);
+        //events not conflicting
+        assertFalse(0 == result);
+        //event 1 less than event 2
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void compareGreaterThanMinute() {
+        event1 = new EventTest(LocalTime.of(1,59), LocalTime.of(2,0), LocalDate.of(2018,1, 2));
+        event2 = new EventTest(LocalTime.of(1,0), LocalTime.of(1,58), LocalDate.of(2018,1, 2));
+        int result = eventComparer.compare(event1,event2);
+        //events not conflicting
+        assertFalse(0 == result);
+        //event 2 less than event 1
+        assertEquals(1, result);
+    }
 }
