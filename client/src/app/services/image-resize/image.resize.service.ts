a0cafd487b279a407342ab529fcc0ac94269f590
emd21 <32820346+emd21@users.noreply.github.com>
2019-03-17 00:55:42 -0400

    #26 Added pica to resize images better, added a cropping method

--- client/src/app/services/image-resize/image.resize.service.ts
+++ client/src/app/services/image-resize/image.resize.service.ts
@@ -1,4 +1,7 @@
 import {ElementRef, Injectable, ViewChild} from '@angular/core';
+// pica can be installed by typing npm install pica
+// more information is available at https://github.com/nodeca/pica
+import pica from 'pica';
 
 @Injectable({
   providedIn: 'root'
@@ -6,14 +9,50 @@ import {ElementRef, Injectable, ViewChild} from '@angular/core';
 export class ImageResizeService {
 
     @ViewChild('myCanvas') myCanvas: ElementRef;
-    public context: CanvasRenderingContext2D;
+    // public context: CanvasRenderingContext2D;
+
+    //expected image sizes for the business profile image and for the user profile image:
+    private businessImgHeight = 250;
+    private businessImageWidth = 200;
+    private userImgHeight = 100;
+    private userImgWidth = 100;
+
+    // ratios between height and width:
+    private businessImageRatio = this.businessImgHeight/this.businessImageWidth;
+    private userImageRatio = this.userImgHeight/this.userImgWidth;
+
   constructor() { }
 
   resizeImage(img, width, height) {
-      this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
-      this.context.drawImage(img, 0, 0, width, height);
-      return this.context.canvas.toDataURL();
+      const resizedCanvas = document.createElement('canvas');
+      resizedCanvas.height = height;
+      resizedCanvas.width = width;
+
+      pica().resize(img, resizedCanvas, {
+              unsharpAmount: 80,
+              unsharpRadius: 0.6,
+              unsharpThreshold: 2
+      }).then(result => {
+          console.log(`resize done!  ${result}`);
+          return result;})
+          .catch(err => console.log(err))
+
   }
+
+  cropImage (img, width, height) {
+      const context: CanvasRenderingContext2D = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
+      return context.drawImage(img, 33, 71, 104, 124, 21, 20, 87, 104);
+  }
+
+    // to properly crop the image, we calculate the ratio
+    // this allows us to crop either the top and bottom or the left and right parts of the image
+    // we crop first and then resize to avoid stretching the image the user provides us
+    imgResize(img){
+      // TO DO: cropping the image before resizing it
+        // calculate the ratio and crop the image as needed, the call the resize function
+
+      //img.naturalHeight
+    }
 }
 
 
