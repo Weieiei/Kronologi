0185fce37ade7790c07afee801e82a62d1dab164
Dylan Fernandes <dylangfernandes@gmail.com>
2019-03-30 18:39:24 -0400

    #56 added shift time tests
    Signed-off-by: Dylan Fernandes <dylangfernandes@gmail.com>

---
+++ server/src/test/java/appointmentscheduler/entity/shift/ShiftTest.java
@@ -0,0 +1,110 @@
+package appointmentscheduler.entity.shift;
+
+import appointmentscheduler.entity.event.AppEvent;
+import appointmentscheduler.entity.event.AppEventTest;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.Assert.*;
+
+public class ShiftTest {
+
+    @Test
+    public void isWithinValid() {
+        LocalDate date = LocalDate.now();
+        LocalTime startTime = LocalTime.of(1,1,1);
+        LocalTime endTime = LocalTime.of(2,2,2);
+
+        Shift shift = new Shift();
+        AppEvent appEvent = new AppEventTest(startTime, endTime, date);
+
+        shift.setDate(date);
+        shift.setStartTime(startTime);
+        shift.setEndTime(endTime);
+
+        assertTrue(shift.isWithin(appEvent));
+    }
+
+    @Test
+    public void isWithinInvalidOverlapWithin() {
+        LocalDate date = LocalDate.now();
+        LocalTime startTime = LocalTime.of(1,1,1);
+        LocalTime endTime = LocalTime.of(8,2,2);
+
+        Shift shift = new Shift();
+        AppEvent appEvent = new AppEventTest(startTime.plusHours(1), endTime.minusHours(1), date);
+
+        shift.setDate(date);
+        shift.setStartTime(startTime);
+        shift.setEndTime(endTime);
+
+        assertTrue(shift.isWithin(appEvent));
+    }
+
+    @Test
+    public void isWithinInvalidBefore() {
+        LocalDate date = LocalDate.now();
+        LocalTime startTime = LocalTime.of(1,1,1);
+        LocalTime endTime = LocalTime.of(2,2,2);
+
+        Shift shift = new Shift();
+        AppEvent appEvent = new AppEventTest(startTime.minusHours(1), startTime, date);
+
+        shift.setDate(date);
+        shift.setStartTime(startTime);
+        shift.setEndTime(endTime);
+
+        assertFalse(shift.isWithin(appEvent));
+    }
+
+    @Test
+    public void isWithinInvalidOverlapBefore() {
+        LocalDate date = LocalDate.now();
+        LocalTime startTime = LocalTime.of(1,1,1);
+        LocalTime endTime = LocalTime.of(2,2,2);
+
+        Shift shift = new Shift();
+        AppEvent appEvent = new AppEventTest(startTime.minusHours(1), endTime, date);
+
+        shift.setDate(date);
+        shift.setStartTime(startTime);
+        shift.setEndTime(endTime);
+
+        assertFalse(shift.isWithin(appEvent));
+    }
+
+    @Test
+    public void isWithinInvalidAfter() {
+        LocalDate date = LocalDate.now();
+        LocalTime startTime = LocalTime.of(1,1,1);
+        LocalTime endTime = LocalTime.of(2,2,2);
+
+        Shift shift = new Shift();
+        shift.setDate(date);
+        shift.setStartTime(startTime);
+        shift.setEndTime(endTime);
+
+        AppEvent appEvent = new AppEventTest(endTime, endTime.plusHours(2), date);
+
+
+        assertFalse(shift.isWithin(appEvent));
+    }
+
+    @Test
+    public void isWithinInvalidOverlapAfter() {
+        LocalDate date = LocalDate.now();
+        LocalTime startTime = LocalTime.of(1,1,1);
+        LocalTime endTime = LocalTime.of(2,2,2);
+
+        Shift shift = new Shift();
+        AppEvent appEvent = new AppEventTest(startTime, endTime.plusHours(2), date);
+
+        shift.setDate(date);
+        shift.setStartTime(startTime);
+        shift.setEndTime(endTime);
+
+        assertFalse(shift.isWithin(appEvent));
+    }
+}
