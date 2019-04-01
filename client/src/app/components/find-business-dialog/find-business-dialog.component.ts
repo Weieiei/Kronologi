da90e0da996a44e4dc38c739de0f8199fd16dc2c
Gibran Khan <jbk5988@gmail.com>
2019-03-31 22:30:51 -0400

    #57 #10 Fixed code formatting and linting errors

--- client/src/app/components/find-business-dialog/find-business-dialog.component.ts
+++ client/src/app/components/find-business-dialog/find-business-dialog.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { BusinessService } from '../../services/business/business.service'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import { BusinessDTO } from "../../interfaces/business/business-dto";
-import { BusinessHoursDTO } from "../../interfaces/business/businessHours-dto";
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { BusinessDTO } from '../../interfaces/business/business-dto';
+import { BusinessHoursDTO } from '../../interfaces/business/businessHours-dto';
 import { trigger, state, style, transition, animate, group, query, stagger, keyframes } from '@angular/animations';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { GeocodingApiService } from '../../services/google/geocode.service'
@@ -55,40 +55,38 @@ import { GeocodingApiService } from '../../services/google/geocode.service'
 `]
 })
 export class FindBusinessDialogComponent implements OnInit {
-  
+
   mapType = 'roadmap';
   form: FormGroup;
-  animationState : boolean = true;
-  businessWasFetched: boolean = false;
-  BusinessName : string
-  businessArray : BusinessDTO[];
-  daysOfWeek : string[] = ["Monday","Tuesday","Wednesday","Thursday", "Friday", "Saturday", "Sunday"];
-  constructor(private geoCodeService : GeocodingApiService, private dialogRef: MatDialogRef<FindBusinessDialogComponent>, private spinner: NgxSpinnerService,private fb : FormBuilder,private businessService : BusinessService,  @Inject(MAT_DIALOG_DATA)public data:any) {
+  animationState = true;
+  businessWasFetched = false;
+  BusinessName: string
+  businessArray: BusinessDTO[];
+  daysOfWeek: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']; constructor(private geoCodeService: GeocodingApiService, private dialogRef:  MatDialogRef<FindBusinessDialogComponent>, private spinner: NgxSpinnerService, private fb: FormBuilder,
+              private businessService: BusinessService,  @Inject(MAT_DIALOG_DATA)public data: any) {
     this.businessArray = [];
   }
-   
 
   ngOnInit() {
   }
 
-
-  searchBusiness(businessName : string):void{
+  searchBusiness(businessName: string): void {
       this.spinner.show();
       this.animationState = false;
       this.businessService.findBusinessThroughGoogle(businessName).subscribe(
         res => {
             console.log(res)
-            for(const business of res){
+            for (const business of res){
               this.updateLatLngFromAddress(business);
-              if(business.business_hours.length < 7){
-                for(const [index, value] of business.business_hours.entries()){
+              if (business.business_hours.length < 7){
+                for (const [index, value] of business.business_hours.entries()){
                   console.log(index);
                   console.log(value);
-                  if(business.business_hours[index].day !== this.daysOfWeek[index]){
+                  if (business.business_hours[index].day !== this.daysOfWeek[index]){
                     let businessHourTemp: BusinessHoursDTO = {
                         day : this.daysOfWeek[index],
-                        openHour: "Close",
-                        closeHour: "Close"
+                        openHour: 'Close',
+                        closeHour: 'Close'
                     }
                     console.log(businessHourTemp);
                     business.business_hours.splice(index, 0, businessHourTemp);
@@ -97,9 +95,9 @@ export class FindBusinessDialogComponent implements OnInit {
               }
               if(business.business_hours.length < 7){
                 let businessHourTemp: BusinessHoursDTO = {
-                  day : "Sunday",
-                  openHour: "Close",
-                  closeHour: "Close"
+                  day : 'Sunday',
+                  openHour: 'Close',
+                  closeHour: 'Close'
               }
                 business.business_hours.push(businessHourTemp);
               }
@@ -108,30 +106,28 @@ export class FindBusinessDialogComponent implements OnInit {
             this.businessWasFetched = true;
             this.animationState = true;
             this.spinner.hide();
-            this.dialogRef.updateSize("600px","600px")
-            
+            this.dialogRef.updateSize('600px','600px');
         },
         err => console.log(err)
       );
   }
 
   updateLatLngFromAddress(business: BusinessDTO) {
-    let businessArr : string[] = business.formattedAddress.split(',');
+    let businessArr: string[] = business.formattedAddress.split(',');
     let address = businessArr[0];
-    let portalCode ="";
-    let selectedPlace = "";
+    let portalCode = '';
+    let selectedPlace = '';
     this.geoCodeService
         .findFromAddress(address, null, null, null, null, null)
         .subscribe(response => {
-            if (response.status == 'OK') {
+            if (response.status === 'OK') {
                 business.lat = response.results[0].geometry.location.lat;
                 business.lng = response.results[0].geometry.location.lng;
-            } else if (response.status == 'ZERO_RESULTS') {
+            } else if (response.status === 'ZERO_RESULTS') {
                 console.log('geocodingAPIService', 'ZERO_RESULTS', response.status);
             } else {
                 console.log('geocodingAPIService', 'Other error', response.status);
             }
         });
-}
-
+  }
 }
