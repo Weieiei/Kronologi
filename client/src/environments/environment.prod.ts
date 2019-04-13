a07985abc0dff7fa59f03f44b0b6e1761c2154ad
marc leclair <marc.leclair0113@gmail.com>
2019-04-13 23:29:54 +0000

    production fix

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/'
+  baseUrl: 'http://kronologi.ca:3000/'
 };
--- server/src/main/java/appointmentscheduler/security/WebSecurityConfig.java
+++ server/src/main/java/appointmentscheduler/security/WebSecurityConfig.java
@@ -185,7 +185,7 @@ public class WebSecurityConfig  {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("http://localhost:4200", "http://localhost");
+                        .allowedOrigins("http://localhost:4200", "http://localhost","http://kronologi.ca","http://kronologi.ca:80","http://kronologi.ca:4200");
             }
 
         };
